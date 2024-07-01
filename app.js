dc1b8062561437631def7d7627a54a79707158ec
cszinn <0000834412@senaimgaluno.com.br>
2024-07-01 19:25:24 -0300

    Modificacao na branch master

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-"console.log('Vers∆o 1.0');" 
-"console.log('Mudanáa na main');" 
+"console.log('Versao 1.0');" 
+<<<<<<< HEAD
+"console.log('Mudanca na main');" 
+=======
+"console.log('Nova funcionalidade');" 
+>>>>>>> 66042f9 (Adiciona nova funcionalidade ao app.js)
+"console.log('Mudanca na master');" 
